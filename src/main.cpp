d1f73d6636f5951275aa3e8f1bf3bfe245e35b5c
eben4ya <benaya.imanuela@gmail.com>
2025-05-28 19:26:23 +0700

    fix: esp pin (wip)

--- src/main.cpp
+++ src/main.cpp
@@ -19,32 +19,32 @@ constexpr int LED_PIN = 2;
 static camera_config_t camera_config = {
     .pin_pwdn = -1,
     .pin_reset = -1,
-    .pin_xclk = 21,
-    .pin_sccb_sda = 26,
-    .pin_sccb_scl = 27,
+    .pin_xclk = 15,
+    .pin_sccb_sda = 4,
+    .pin_sccb_scl = 5,
 
-    .pin_d7 = 35,
-    .pin_d6 = 34,
-    .pin_d5 = 39,
-    .pin_d4 = 36,
-    .pin_d3 = 19,
-    .pin_d2 = 18,
-    .pin_d1 = 5,
-    .pin_d0 = 4,
+    .pin_d7 = 16,
+    .pin_d6 = 17,
+    .pin_d5 = 18,
+    .pin_d4 = 12,
+    .pin_d3 = 10,
+    .pin_d2 = 8,
+    .pin_d1 = 9,
+    .pin_d0 = 11,
 
-    .pin_vsync = 25,
-    .pin_href = 23,
-    .pin_pclk = 22,
+    .pin_vsync = 6,
+    .pin_href = 7,
+    .pin_pclk = 13,
 
-    .xclk_freq_hz = 20000000,
+    .xclk_freq_hz = 10000000,
     .ledc_timer = LEDC_TIMER_0,
     .ledc_channel = LEDC_CHANNEL_0,
 
     .pixel_format = PIXFORMAT_JPEG,
-    .frame_size = FRAMESIZE_VGA,
+    .frame_size = FRAMESIZE_UXGA,
     .jpeg_quality = 12,
     .fb_count = 1};
-
+    
 // Set LED blink
 void blinkLED(int times)
 {
